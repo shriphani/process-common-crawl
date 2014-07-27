9ba766385bab79738625edae792cb8464f3405f0
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-27 19:04:28 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to process_common_crawl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/process_common_crawl/core_test.clj
@@ -0,0 +1,7 @@
+(ns process-common-crawl.core-test
+  (:require [clojure.test :refer :all]
+            [process-common-crawl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
